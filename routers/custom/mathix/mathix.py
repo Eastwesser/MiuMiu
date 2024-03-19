9d855d237129d33cb1424eafe0c7a20214202da4
Dennis Mattews <alttert@mail.ru>
2024-03-19 22:48:31 +0300

    MiuMiu 0.1.6 improved mathix kb readability

--- routers/custom/mathix/mathix.py
+++ routers/custom/mathix/mathix.py
@@ -139,22 +139,57 @@ conversion_functions = {
 }
 
 
-# Create a keyboard for the conversion menu
 def create_keyboard(commands):
     buttons_row_1 = [
-        InlineKeyboardButton(text="INCHES", callback_data="/inches_options"),
-        InlineKeyboardButton(text="MILES", callback_data="/miles_options"),
-        InlineKeyboardButton(text="POUNDS", callback_data="/pounds_options"),
+        InlineKeyboardButton(
+            text="INCHES",
+            callback_data="/inches_options"
+        ),
+
+        InlineKeyboardButton(
+            text="MILES",
+            callback_data="/miles_options"
+        ),
+
+        InlineKeyboardButton(
+            text="POUNDS",
+            callback_data="/pounds_options"
+        ),
+
     ]
     buttons_row_2 = [
-        InlineKeyboardButton(text="F°", callback_data="/fahrenheit_options"),
-        InlineKeyboardButton(text="OUNCES", callback_data="/ounces_options"),
-        InlineKeyboardButton(text="GALLONS", callback_data="/gallons_options"),
+        InlineKeyboardButton(
+            text="F°",
+            callback_data="/fahrenheit_options"
+        ),
+
+        InlineKeyboardButton(
+            text="OUNCES",
+            callback_data="/ounces_options"
+        ),
+
+        InlineKeyboardButton(
+            text="GALLONS",
+            callback_data="/gallons_options"
+        ),
+
     ]
     buttons_row_3 = [
-        InlineKeyboardButton(text="FEET", callback_data="/feet_options"),
-        InlineKeyboardButton(text="YARDS", callback_data="/yards_options"),
-        InlineKeyboardButton(text="CUPS", callback_data="/cups_options"),
+        InlineKeyboardButton(
+            text="FEET",
+            callback_data="/feet_options"
+        ),
+
+        InlineKeyboardButton(
+            text="YARDS",
+            callback_data="/yards_options"
+        ),
+
+        InlineKeyboardButton(
+            text="CUPS",
+            callback_data="/cups_options"
+        ),
+
     ]
     return InlineKeyboardMarkup(inline_keyboard=[
         buttons_row_1,
@@ -163,59 +198,126 @@ def create_keyboard(commands):
     ])
 
 
-# Define a dictionary to store the additional keyboards for each conversion command
 additional_keyboards = {
     "/inches_options": [
-        InlineKeyboardButton(text="Inches to Centimeters", callback_data="/inches_to_cm"),
-        InlineKeyboardButton(text="Centimeters to Inches", callback_data="/cm_to_inches"),
+        InlineKeyboardButton(
+            text="Inches to Centimeters",
+            callback_data="/inches_to_cm"
+        ),
+
+        InlineKeyboardButton(
+            text="Centimeters to Inches",
+            callback_data="/cm_to_inches"
+        ),
+
     ],
     "/miles_options": [
-        InlineKeyboardButton(text="Miles to Kilometers", callback_data="/miles_to_km"),
-        InlineKeyboardButton(text="Kilometers to Miles", callback_data="/km_to_miles"),
+        InlineKeyboardButton(
+            text="Miles to Kilometers",
+            callback_data="/miles_to_km"
+        ),
+
+        InlineKeyboardButton(
+            text="Kilometers to Miles",
+            callback_data="/km_to_miles"
+        ),
+
     ],
     "/pounds_options": [
-        InlineKeyboardButton(text="Pounds to Kilograms", callback_data="/pounds_to_kg"),
-        InlineKeyboardButton(text="Kilograms to Pounds", callback_data="/kg_to_pounds"),
+        InlineKeyboardButton(
+            text="Pounds to Kilograms",
+            callback_data="/pounds_to_kg"
+        ),
+
+        InlineKeyboardButton(
+            text="Kilograms to Pounds",
+            callback_data="/kg_to_pounds"
+        ),
+
     ],
     "/fahrenheit_options": [
-        InlineKeyboardButton(text="Fahrenheit to Celsius", callback_data="/fahrenheit_to_celsius"),
-        InlineKeyboardButton(text="Celsius to Fahrenheit", callback_data="/celsius_to_fahrenheit"),
+        InlineKeyboardButton(
+            text="Fahrenheit to Celsius",
+            callback_data="/fahrenheit_to_celsius"
+        ),
+
+        InlineKeyboardButton(
+            text="Celsius to Fahrenheit",
+            callback_data="/celsius_to_fahrenheit"
+        ),
+
     ],
     "/ounces_options": [
-        InlineKeyboardButton(text="Ounces to Milliliters", callback_data="/ounces_to_ml"),
-        InlineKeyboardButton(text="Milliliters to Ounces", callback_data="/ml_to_ounces"),
+        InlineKeyboardButton(
+            text="Ounces to Milliliters",
+            callback_data="/ounces_to_ml"
+        ),
+
+        InlineKeyboardButton(
+            text="Milliliters to Ounces",
+            callback_data="/ml_to_ounces"
+        ),
+
     ],
     "/gallons_options": [
-        InlineKeyboardButton(text="Gallons to Liters", callback_data="/gallons_to_liters"),
-        InlineKeyboardButton(text="Liters to Gallons", callback_data="/liters_to_gallons"),
+        InlineKeyboardButton(
+            text="Gallons to Liters",
+            callback_data="/gallons_to_liters"
+        ),
+
+        InlineKeyboardButton(
+            text="Liters to Gallons",
+            callback_data="/liters_to_gallons"
+        ),
+
     ],
     "/feet_options": [
-        InlineKeyboardButton(text="Feet to Meters", callback_data="/feet_to_meters"),
-        InlineKeyboardButton(text="Meters to Feet", callback_data="/meters_to_feet"),
+        InlineKeyboardButton(
+            text="Feet to Meters",
+            callback_data="/feet_to_meters"
+        ),
+
+        InlineKeyboardButton(
+            text="Meters to Feet",
+            callback_data="/meters_to_feet"
+        ),
+
     ],
     "/yards_options": [
-        InlineKeyboardButton(text="Yards to Meters", callback_data="/yards_to_meters"),
-        InlineKeyboardButton(text="Meters to Yards", callback_data="/meters_to_yards"),
+        InlineKeyboardButton(
+            text="Yards to Meters",
+            callback_data="/yards_to_meters"
+        ),
+
+        InlineKeyboardButton(
+            text="Meters to Yards",
+            callback_data="/meters_to_yards"
+        ),
+
     ],
     "/cups_options": [
-        InlineKeyboardButton(text="Cups to Liters", callback_data="/cups_to_liters"),
-        InlineKeyboardButton(text="Liters to Cups", callback_data="/liters_to_cups"),
+        InlineKeyboardButton(
+            text="Cups to Liters",
+            callback_data="/cups_to_liters"
+        ),
+
+        InlineKeyboardButton(
+            text="Liters to Cups",
+            callback_data="/liters_to_cups"
+        ),
     ],
 }
 
 
-# Handle the /converter command to display the conversion menu
 @router.message(Command("converter", prefix="/!%"))
 async def converter_menu(message: types.Message):
     keyboard = create_keyboard(list(additional_keyboards.keys()))
     await message.reply("Choose a conversion:", reply_markup=keyboard)
 
 
-# Define a variable to store the last conversion command
 last_conversion_command = None
 
 
-# Handle callback queries from the conversion menu
 @router.callback_query()
 async def handle_conversion_query(callback_query: types.CallbackQuery):
     global last_conversion_command
@@ -231,7 +333,6 @@ async def handle_conversion_query(callback_query: types.CallbackQuery):
         await callback_query.message.answer("Invalid conversion option!")
 
 
-# Handle messages containing only numbers
 @router.message(
     lambda message: message.text.strip().replace('.', '', 1).isdigit()
                     and last_conversion_command is not None
@@ -242,26 +343,32 @@ async def handle_numbers(message: types.Message):
         try:
             number = float(message.text)
             result = conversion_functions[last_conversion_command](number)
-            # Round the result to 1 decimal place
             rounded_result = round(result, 1)
-            # Additional info for specific functions
             additional_info = {
                 "/inches_to_cm": f"Your result is {rounded_result} cm.",
                 "/cm_to_inches": f"Your result is {rounded_result} inches.",
+
                 "/miles_to_km": f"Your result is {rounded_result} km.",
                 "/km_to_miles": f"Your result is {rounded_result} miles.",
+
                 "/pounds_to_kg": f"Your result is {rounded_result} kg.",
                 "/kg_to_pounds": f"Your result is {rounded_result} pounds.",
+
                 "/fahrenheit_to_celsius": f"Your result is {rounded_result} °C.",
                 "/celsius_to_fahrenheit": f"Your result is {rounded_result} °F.",
+
                 "/ounces_to_ml": f"Your result is {rounded_result} mL.",
                 "/ml_to_ounces": f"Your result is {rounded_result} ounces.",
+
                 "/gallons_to_liters": f"Your result is {rounded_result} liters.",
                 "/liters_to_gallons": f"Your result is {rounded_result} gallons.",
+
                 "/feet_to_meters": f"Your result is {rounded_result} meters.",
                 "/meters_to_feet": f"Your result is {rounded_result} feet.",
+
                 "/yards_to_meters": f"Your result is {rounded_result} meters.",
                 "/meters_to_yards": f"Your result is {rounded_result} yards.",
+
                 "/cups_to_liters": f"Your result is {rounded_result} liters.",
                 "/liters_to_cups": f"Your result is {rounded_result} cups.",
             }
